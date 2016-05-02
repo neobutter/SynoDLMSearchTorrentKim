98c083ce927d56c3e958c3e961f8aee8daff367d
Changyong Park <cypark@hivetech.kr>
2016-05-02 16:27:23 +0900

    Remove legacy code And use addRSSResults helper function

--- Src/search.php
+++ Src/search.php
@@ -11,37 +11,7 @@ class SynoDLMSearchTorrentKim {
 	}
 
 	public function parse($plugin, $response) {
-		$regexp = "<tr class=\"bg.*bo_table=torrent_(.*)&wr_id=(.*)\">.*<\/tr>";
-		$res=0;
-		if(preg_match_all("/$regexp/siU", $response, $matches, PREG_SET_ORDER)) {
-			$title="Unknown title";
-			$download="Unknown download";
-			$size=0;
-			$datetime="1900-12-31";
-			$page="Default page";
-			$hash="Hash unknown";
-			$seeds=0;
-			$leechs=0;
-			$category="Unknown category";
-
-			foreach($matches as $match) {
-					$categoryv = $match[1];
-					$id        = $match[2];
-					$info      = $this->getInfo($categoryv, $id);
-					$title     = $info['title'   ];
-					$download  = $info['download'];
-					$size      = $info['size'    ];
-					$datetime  = $info['date'    ];
-					$category  = $info['category'];
-					$hash      = md5($res.$title);
-					$seeds     = 0;
-					$leechs    = 0;
-					$page      = sprintf($this->purl, $categoryv, $id);
-					$plugin->addResult($title, $download, $size, $datetime, $page, $hash, $seeds, $leechs, $category);
-					$res++;
-			}
-		}
-		return $res;
+		return $plugin->addRSSResults($response);
 	}
 }
 ?>
