7ce7d511115e0ed47e4d6a623e440ef210594443
cypark <cypark@theworkshop.com>
2016-07-20 15:00:09 +0900

    - Changed file permission - Removed some unnecessary code

--- src/search.php
+++ src/search.php
@@ -8,18 +8,17 @@ class SynoDLMSearchTorrentKim {
 	public function prepare($curl, $query) {
 		$url = $this->qurl . urlencode($query);
 		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_REFERER, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER , TRUE);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+		// curl_setopt($curl, CURLOPT_REFERER, $url);
+		// curl_setopt($curl, CURLOPT_RETURNTRANSFER , TRUE);
+		// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
 	}
 
 	public function parse($plugin, $response) {
-		$response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" . $response;
 		$response = preg_replace("/<pubDate>/i", "<pubDate>" . date("r"), $response);
 		$response = preg_replace("/<\/pubDate>/i", "</pubDate><category>All</category>", $response);
 		$response = preg_replace("/\&/i", "&amp;", $response);
-		$response = preg_replace("/<description><\/description>/i", "<description><![CDATA[Category: All<br />Subcategory: All<br />Size: 100&nbsp;kilobyte<br />Ratio: 1 seeds, 0 leechers<br />Language: Unknown<br />Uploaded by: Unknown]]></description>", $response);
+		$response = preg_replace("/<description><\/description>/i", "<description><![CDATA[Category: All<br />Subcategory: All<br />Size: 100&nbsp;kilobyte<br />Language: Unknown<br />Uploaded by: Unknown]]></description>", $response);
 		$response = rawurldecode($response);
 		return $plugin->addRSSResults($response);
 	}
