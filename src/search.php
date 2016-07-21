2d3db567b3a330fec34560a1332815f03db8261b
neobutter <neobutter@me.com>
2016-07-22 03:35:51 +0900

    - Fixed ios issue

--- src/search.php
+++ src/search.php
@@ -8,19 +8,14 @@ class SynoDLMSearchTorrentKim {
 	public function prepare($curl, $query) {
 		$url = $this->qurl . urlencode($query);
 		curl_setopt($curl, CURLOPT_URL, $url);
-		// curl_setopt($curl, CURLOPT_REFERER, $url);
-		// curl_setopt($curl, CURLOPT_RETURNTRANSFER , TRUE);
-		// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-		// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
 	}
 
 	public function parse($plugin, $response) {
 		$response = preg_replace("/<pubDate>/i", "<pubDate>" . date("r"), $response);
 		$response = preg_replace("/<\/pubDate>/i", "</pubDate><category>All</category>", $response);
-		$response = preg_replace("/\&/i", "&amp;", $response);
-		$response = preg_replace("/<description><\/description>/i", "<description><![CDATA[Category: All<br />Subcategory: All<br />Size: 100&nbsp;kilobyte<br />Language: Unknown<br />Uploaded by: Unknown]]></description>", $response);
-		$response = preg_replace("/<enclosure[^>]*url=[\"']?([^>\"']+)[\"']?[^>]*>/i", "<enclosure url=\"" . "http://www.mininova.org/get/13265623" . "\" length=\"0\" type=\"application/x-bittorrent\" />", $response);
-		// $response = rawurldecode($response);
+		$response = preg_replace("/<description><\/description>/i", "<description><![CDATA[Category: All<br />Subcategory: All]]></description>", $response);
+		$response = preg_replace("/\&dn=.*?(?=&tr)/i", "", $response);
+		$response = preg_replace("/&/i", "%26", $response);
 		return $plugin->addRSSResults($response);
 	}
 }
